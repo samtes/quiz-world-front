1710e68913f46a49f6bb4c836802d1e81f142101
samtes <sweldemariam@gmail.com>
2015-12-31 16:31:30 -0800

    refactor(route): Modifies login and register routes the ember way

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  setupController: function(controller, model) {
+    this.controllerFor("application").setProperties({
+      title: "Login Page",
+      subTitles: [
+        { title: "LOGIN", class: "active", page: "login" },
+        { title: "REGISTER", class: "inactive", page: "register" }
+      ]
+    });
+  }
 });
--- app/routes/register.js
+++ app/routes/register.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  setupController: function(controller, model) {
+    this.controllerFor("application").setProperties({
+      title: "Registration Page",
+      subTitles: [
+        { title: "LOGIN", class: "inactive", page: "login" },
+        { title: "REGISTER", class: "active", page: "register" }
+      ]
+    });
+  }
 });
