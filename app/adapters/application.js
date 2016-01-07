3521401fb488c4e200cbd2eda0faa5f8380b3e5f
samtes <sweldemariam@gmail.com>
2016-01-06 21:09:10 -0800

    fix(adapter): Use config to setup host in adapters/application.js

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,8 @@
 import DS from "ember-data";
+import config from "quiz-world-front/config/environment";
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.RESTAdapter.extend(DataAdapterMixin, {
-    host: 'http://localhost:4500',
-    authorizer: 'authorizer:oauth2'
+  authorizer: 'authorizer:oauth2',
+  host: config.REST_ENDPOINT
 });
