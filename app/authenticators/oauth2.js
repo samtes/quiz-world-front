4516a183f4d1443ad97dde579351ef3f6af5a21e
samtes <sweldemariam@gmail.com>
2016-01-06 21:16:10 -0800

    fix(authenticators): Uses ENV to setup endpoint

--- app/authenticators/oauth2.js
+++ app/authenticators/oauth2.js
@@ -1,5 +1,6 @@
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
+import ENV from "quiz-world-front/config/environment";
 
 export default OAuth2PasswordGrant.extend({
-  serverTokenEndpoint: 'http://localhost:4500/login'
+  serverTokenEndpoint: ENV.LOGIN_TOKEN_ENDPOINT
 });
